ed7a3a8a2ed103bc5b0d960407b6bf0ebb0a50f8
Guofang Xiao <guofangxiao@hotmail.com>
2021-10-08 23:12:23 +0100

    First commit from RStudio.

---
+++ firstRscript.R
@@ -0,0 +1,3 @@
+print("Finally cloned the repository from GitHub using SSH URL.")
+print("At first access was denied due to firewall protection.")
+print("Creating a config file under .ssh directory solved the problem.")
